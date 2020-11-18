aec5d7bdddedcfb6887643517b16520611ec2520
Elias Campos <ecampose76@gmail.com>
2020-11-18 00:08:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,8 +4,10 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
     #binding.pry
     value.each do |inner_key, names|
-      binding.pry
-      
+      #binding.pry
+      names.each do |name|
+        binding.pry
+      end
     end
   end
  # binding.pry
