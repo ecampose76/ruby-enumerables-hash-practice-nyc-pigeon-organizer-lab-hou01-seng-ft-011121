df526b86d3f746d414a54a026fda880ab20dbd2c
Elias Campos <ecampose76@gmail.com>
2020-11-18 00:22:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,7 +10,6 @@ def nyc_pigeon_organizer(data)
         if !final_array[name]
           final_array[name] = {}
         end
-
         if !final_array[name][key]
           !final_array[name][key] = []
       end
